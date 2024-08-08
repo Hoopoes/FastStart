2c1c0244bc14052c52c279f7f8557e25f9914a08
umar-anzar <63141196+umar-anzar@users.noreply.github.com>
2024-08-09 00:01:41 +0500

    fix: custom_log file already exist error fixed, and previous date log filename fixed

--- app/utils/custom_log.py
+++ app/utils/custom_log.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from app.utils.logger import LOG_DIRECTORY
 from logging.handlers import TimedRotatingFileHandler
 
@@ -14,11 +14,18 @@ class CustomTimedRotatingFileHandler(TimedRotatingFileHandler):
             self.stream = None
         
         # Determine the new file name
-        current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        current_time = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
         new_filename = os.path.join(LOG_DIRECTORY, f"{current_time}.log")
+        
+        # Check if the new filename already exists
+        if os.path.exists(new_filename):
+            os.remove(new_filename)
         
         # Rename the current log file
         os.rename(self.baseFilename, new_filename)
         
         # Create a new log file
         self.stream = self._open()
+        
+        # Update the last rollover time
+        self.rolloverAt = self.rolloverAt + self.interval
