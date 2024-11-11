e3c5ef14411d319a21f7805f16d76062f9300311
umar-anzar <omer.anzar2@gmail.com>
2024-11-11 20:26:01 +0500

    chore: config file updated

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 from os import getenv
+from typing import Optional
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
 
 load_dotenv()
 
 
-class Config:
-    app_name = "<APP_NAME>"
-    description = "<APP_DESCRIPTION>"
-    api_key = getenv("API_KEY")
+class ConfigClass(BaseModel):
+    app_name: str
+    description: str
+    api_key: Optional[str]
+
+Config = ConfigClass(
+    app_name = "<APP_NAME>",
+    description = "<APP_DESCRIPTION>",
+    api_key = getenv("API_KEY") if getenv("API_KEY") else None
+)
