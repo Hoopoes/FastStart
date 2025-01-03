b3a45c395771904c521ea4c564d6cb1105d63cbb
umar-anzar <omer.anzar2@gmail.com>
2025-01-03 22:29:35 +0500

    chore: tomllib parser added for app name description and version

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import tomllib
 from os import getenv
 from typing import Optional
 from dotenv import load_dotenv
@@ -6,16 +7,23 @@ from pydantic import BaseModel
 
 load_dotenv()
 
+with open("pyproject.toml", "rb") as f:
+    poetry: dict[str] = tomllib.load(f).get("tool", {}).get("poetry", {})
+    name = poetry.get("name")
+    description = poetry.get("description")
+    version = poetry.get("version")
 
 class ConfigClass(BaseModel):
     app_name: str
     description: str
+    version: str
     root_path: str
     api_key: Optional[str]
 
 CONFIG = ConfigClass(
-    app_name = "<APP_NAME>",
-    description = "<APP_DESCRIPTION>",
+    app_name = name,
+    description = description,
+    version = version,
     root_path="/api/v1",
     api_key = getenv("API_KEY") if getenv("API_KEY") else None
 )
