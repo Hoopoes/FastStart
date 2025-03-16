cdeee9f055828e462333bb12143f7923ab03844a
umar-anzar <omer.anzar2@gmail.com>
2025-03-16 23:13:41 +0500

    chore: response code converted to string

--- app/schema/base_schema.py
+++ app/schema/base_schema.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 
 
 class BaseResponse(BaseModel):
-    resp_code: int
-    response_description: str
+    resp_code: str
+    resp_description: str
 
 
 # Global response definitions
@@ -12,7 +12,7 @@ GLOBAL_RESPONSES = {
         "description": "Validation Error",
         "content": {
             "application/json": {
-                "example": BaseResponse(resp_code=422, response_description="Field 'name' - This field is required").model_dump()
+                "example": BaseResponse(resp_code="REQ_VALIDATION_FAILED", resp_description="Field 'name' - This field is required").model_dump()
             }
         },
     },
@@ -20,7 +20,7 @@ GLOBAL_RESPONSES = {
         "description": "Internal Server Error",
         "content": {
             "application/json": {
-                "example": BaseResponse(resp_code=500, response_description="Internal Server Error").model_dump()
+                "example": BaseResponse(resp_code="INTERNAL_SERVER_ERROR", resp_description="Internal Server Error").model_dump()
             }
         },
     },
