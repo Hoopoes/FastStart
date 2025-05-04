6338e2409d6fbcc10472485498bde1bfe7a4129c
umar-anzar <omer.anzar2@gmail.com>
2025-05-04 13:39:34 +0500

    chore: log viewer data fetch api created
    chore: log viewer html is converted from serverside to client side rendering

--- app/routes/dev/endpoints/log_viewer.py
+++ app/routes/dev/endpoints/log_viewer.py
@@ -1,7 +1,8 @@
-import json
 from datetime import datetime
+from pydantic import BaseModel
+from fastapi import APIRouter, Query
 from fastapi.responses import HTMLResponse
-from fastapi import APIRouter, Query, HTTPException
+
 
 from app.services.log_viewer_service import read_log_file
 
@@ -10,27 +11,34 @@ from app.services.log_viewer_service import read_log_file
 log_router = APIRouter()
 
 
-@log_router.get("/logs", response_class=HTMLResponse)
-def view_logs(date: str = Query("today", description="Date in YYYY-MM-DD format or 'today'")):
+class LogData(BaseModel):
+    columns: list[str]
+    logs: list[dict]
+    date: datetime
 
+
+@log_router.get("/logs/data")
+def logs_data(date: str = Query("today", description="Date in YYYY-MM-DD format or 'today'")) -> LogData:
+    
     logs, date = read_log_file(date)
 
     all_keys = set()
     for log in logs:
         all_keys.update(log.keys())
-    headers = sorted(list(all_keys))  # Sort headers for consistency
+    columns = sorted(list(all_keys))  # Sort headers for consistency
     
     if date == "today":
         log_date = datetime.today().strftime("%Y-%m-%d")
     else:
         log_date = date  # Already in YYYY-MM-DD format
     
+    return LogData(columns=columns, logs=logs, date=log_date)
+    
+
+@log_router.get("/logs", response_class=HTMLResponse)
+def view_logs():
 
     with open("app/templates/logger_view.html", 'r') as file:
         html_content = file.read()
-        html_content= html_content.replace("@{date}", json.dumps(log_date))\
-            .replace("@{logs}", json.dumps(logs, ensure_ascii=False))\
-            .replace("@{header_list}", json.dumps(headers, ensure_ascii=False))
         
-                
     return HTMLResponse(content=html_content)
